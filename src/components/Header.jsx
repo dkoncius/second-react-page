74c26c681d167b43840a4cf9374856c9e50cc1b3
dkoncius <as.koncius@gmail.com>
2023-12-29 09:38:59 +0200

    update

---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+import "../styles/Header.css"
+
+const Header = () => {
+  return (
+    <>
+        <header>
+            <div className="container">
+                <h1>Title</h1>
+                <p>Some about text</p>
+                <p>in two lines</p>
+                <button>Button</button>
+            </div>
+        </header>
+    </>
+  )
+}
+
+export default Header
